c4bb7c272bde294c6ff55dff1f3847d26ddbb204
linfuliang <1423835668@qq.com>
2024-05-15 12:05:35 +0800

    feat-(commit build)

--- packages/play/src/main.ts
+++ packages/play/src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import ToElement from "toy-element";
+import "toy-element/dist/index.css";
 
 createApp(App).use(ToElement).mount('#app')
